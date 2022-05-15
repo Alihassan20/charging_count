c274576230253975bd8422101ff868900f14a3d7
Alihassan20 <43120067+Alihassan20@users.noreply.github.com>
2022-05-15 16:34:37 +0400

    final commet

--- lib/feature/charging_page/view.dart
+++ lib/feature/charging_page/view.dart
@@ -1,3 +1,4 @@
+import 'package:charging_count/feature/charging_page/resuit/view.dart';
 import 'package:flutter/material.dart';
 
 class ChargingPage extends StatefulWidget {
@@ -8,24 +9,70 @@ class ChargingPage extends StatefulWidget {
 }
 
 class _ChargingPageState extends State<ChargingPage> {
+  TextEditingController _controller = TextEditingController();
+  var value;
+  late double charge;
+  late double pound;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("إشحن بكام"),
       ),
-      body: Column(
-        children: [
-          Text("أدخل قيمة الشحن "),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'القيمة',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15)
-              )
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("أدخل قيمة الشحن "),
+                SizedBox(height: 20),
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'القيمة',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)
+                    )
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 200,
+                  color: Colors.blue,
+                  child: TextButton(
+                      onPressed: () {
+                        value = _controller.text;
+                        value = double.parse(value);
+                        charge = value * (70 / 100);
+                        pound = value * 100 / 70;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChargingResult(
+                                value: value.toString(),
+                                pound: pound,
+                                charge: charge),
+                          ),
+                        );
+                        //amountController.text=charge.toString();
+                      },
+                      child: const Text(
+                        'احسب',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      )),
+                )
+
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
